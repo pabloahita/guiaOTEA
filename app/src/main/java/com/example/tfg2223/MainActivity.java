8d66a5a29e2088a839e24047de48e51fb4145f44
Pablo <pabloahitami@hotmail.es>
2023-01-25 00:40:47 +0100

    Inicialización del proyecto

---
+++ app/src/main/java/com/example/tfg2223/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tfg2223;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
