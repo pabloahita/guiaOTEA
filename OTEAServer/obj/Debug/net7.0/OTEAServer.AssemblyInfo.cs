911a197715de0f12265e8d132a89bbfe4d2b84cc
unknown <pabloahitami@hotmail.es>
2024-02-28 22:24:16 +0100

    Regreso a ultima version bien

--- OTEAServer/obj/Debug/net7.0/OTEAServer.AssemblyInfo.cs
+++ OTEAServer/obj/Debug/net7.0/OTEAServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTEAServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42447de60acab6366bbfabb9ffd44504eab598f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTEAServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTEAServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
